a6d225a5323d0576426dadc4d372ad7d3f77aa27
D3rK0nig45 <32471143+D3rK0nig45@users.noreply.github.com>
2024-09-14 21:42:24 -0300

    Final

--- scripts.js
+++ scripts.js
@@ -5,12 +5,18 @@ async function getWeather(){
     const key = "J963KSZFK5CSLNH9WEQTZB5U5";
     const city = document.querySelector("#city").value;
     const weatherAPI = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${key}&contentType=json`;
-    const res = await fetch(weatherAPI);
-    const data = await res.json();
-    const weather = await data.description;
-    const cityname = await data.address;
-    info.innerHTML = "";
-    info.innerHTML = `<p>${weather}</p><p>${cityname}</p>`;
+    try{
+        const res = await fetch(weatherAPI);
+        const data = await res.json();
+        const weather = await data.description;
+        const cityname = await data.address;
+        info.innerHTML = "";
+        info.innerHTML = `<p>${weather}</p><p>${cityname}</p>`;
+    }catch(err){
+        console.error(err);
+        info.innerHTML = "Error fetching weather data.";
+        return;
+    }
 }
 search.addEventListener("click", function(){
     getWeather();
